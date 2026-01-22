5ea85b5756caeaa71c00bd6afd6513d2e183e00a
byako <rlkira94@gmail.com>
2026-01-22 17:24:06 +0100

    Init project

---
+++ src/main/java/com/project/task/TaskAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAppApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TaskAppApplication.class, args);
+	}
+
+}
