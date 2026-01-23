9f3e4854b79f8cc407790bf6aec9ff1ab8e57700
byako <rlkira94@gmail.com>
2026-01-23 15:23:31 +0100

    Create task repository

---
+++ src/main/java/com/project/task/repository/TaskRepository.java
@@ -0,0 +1,12 @@
+package com.project.task.repository;
+
+import com.project.task.domain.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+
+}
